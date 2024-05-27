89f634318b31099ee6ea62694f8495c1d0f5864e
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2024-05-27 15:26:49 +0000

    Adapt to newest architecture

--- thingsboard/__init__.py
+++ thingsboard/__init__.py
@@ -1,5 +1,7 @@
 """The thingsboard integration."""
 from __future__ import annotations
+import json
+import hashlib
 from dataclasses import dataclass
 from datetime import datetime
 from homeassistant.config_entries import ConfigEntry
@@ -7,19 +9,24 @@ from homeassistant.core import Event, HomeAssistant, State
 from homeassistant.const import (
     MATCH_ALL
 )
-
-import hashlib
 import paho.mqtt.client as mqtt
-import json
-
-
-@dataclass
-class StateCache:
-    last_updated: datetime
-    last_changed: datetime
 
 
 async def get_platform_from_entity_id(hass, entity_id):
+    """
+    Asynchronously retrieves the platform of an entity in the Home Assistant ecosystem.
+
+    This function fetches the platform of a specified entity by its entity ID. If the entity
+    is found in the entity registry, the function returns its associated platform. If the entity
+    is not found, the function returns None.
+
+    Args:
+        hass: The HomeAssistant instance, used to access the entity registry.
+        entity_id (str): The unique identifier of the entity whose platform is to be retrieved.
+
+    Returns:
+        str or None: The platform of the specified entity if found, otherwise None.
+    """
     entity_registry = hass.helpers.entity_registry.async_get(hass)
     entity = entity_registry.async_get(entity_id)
 
@@ -29,6 +36,21 @@ async def get_platform_from_entity_id(hass, entity_id):
 
 
 async def get_device_id(hass: HomeAssistant, entity_id: str) -> str:
+    """
+    Publishes the state of a device to a specific topic in the MQTT broker.
+
+    This function serializes the state of a device along with its device class and publishes it
+    to the 'v1/gateway/telemetry' topic. The data is formatted in JSON with the device ID as the key.
+
+    Args:
+        client: The MQTT client used for publishing the message.
+        device_id (str): The ID of the device whose state is being published.
+        state: The state object of the device, expected to have an 'as_dict' method.
+        device_class (str): The class of the device (e.g., sensor, light).
+
+    Returns:
+        None
+    """
     entity_registry = hass.helpers.entity_registry.async_get(hass)
     entity = entity_registry.async_get(entity_id)
 
@@ -36,37 +58,102 @@ async def get_device_id(hass: HomeAssistant, entity_id: str) -> str:
         if entity.device_id is not None:
             return entity.device_id
 
-    return None, None
+    return None
 
 
-def publishState(client, device_id, state, device_class):
-    client.publish('v1/gateway/telemetry', json.dumps({
+def publish_connect(client, device_id, device_class):
+    """
+    Connects the device to Thingsboard.
+
+    Args:
+        client: The MQTT client used for publishing the message.
+        device_id (str): The ID of the device whose state is being published.
+        device_class (str): The class of the device (e.g., sensor, light).
+
+    Returns:
+        None
+    """
+    return client.publish('v1/gateway/connect', json.dumps({
+        'device': device_id,
+        'type': device_class
+    }))
+
+
+def publish_state(client, device_id, state, device_class):
+    """
+    Publishes the state of a device to a specific topic in the Thingsboard MQTT broker.
+
+    This function serializes the state of a device along with its device class and publishes it
+    to the 'v1/gateway/telemetry' topic. The data is formatted in JSON with the device ID as the key.
+
+    Args:
+        client: The MQTT client used for publishing the message.
+        device_id (str): The ID of the device whose state is being published.
+        state: The state object of the device, expected to have an 'as_dict' method.
+        device_class (str): The class of the device (e.g., sensor, light).
+
+    Returns:
+        None
+    """
+    return client.publish('v1/gateway/telemetry', json.dumps({
         device_id: [{
             device_class: state.as_dict()['state']
         }]
     }))
 
 
-def publishAttributes(client, device_id, attributes):
-    client.publish('v1/gateway/attributes', json.dumps({
+def publish_attributes(client, device_id, attributes):
+    """
+    Publishes attributes of a device to a specific topic in the Thingsboard MQTT broker.
+
+    This function takes the attributes of a device and publishes them to the
+    'v1/gateway/attributes' topic in the MQTT broker. The data is formatted in JSON with
+    the device ID as the key.
+
+    Args:
+        client: The MQTT client used for publishing the message.
+        device_id (str): The ID of the device whose attributes are being published.
+        attributes (dict): A dictionary of the attributes to be published.
+
+    Returns:
+        None
+    """
+    return client.publish('v1/gateway/attributes', json.dumps({
         device_id: attributes
     }))
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """
+    Set up an entry for ThingsBoard integration.
+
+    This function is called by Home Assistant when setting up a ThingsBoard integration entry.
+    It establishes a connection to the ThingsBoard MQTT broker and listens for state events
+    from sensors specified in the entry's configuration.
+
+    Args:
+        hass (HomeAssistant): The Home Assistant instance.
+        entry (ConfigEntry): The ThingsBoard integration entry.
+
+    Returns:
+        bool: True if the setup was successful, False otherwise.
+    """
     client = mqtt.Client("home-assistant", protocol=mqtt.MQTTv31)
     client.username_pw_set(entry.data.get('access_token'), password=None)
+    if entry.data.get('tls'):
+        client.tls_set()
     client.connect(entry.data.get('host'), entry.data.get('port'))
-    entity_id_cache = dict()
+    entity_id_cache = {}
 
     async def state_event_listener(event: Event) -> None:
         if state := event.data.get("new_state"):
             device_class = state.attributes.get('device_class')
-            if device_class and state.domain == "sensor":
+            if device_class and state.domain == "sensor" and device_class in entry.data.get('sensors'):
                 entity_id = hashlib.sha1(event.data.get(
                     'entity_id').encode('utf-8')).hexdigest()
                 device_id = await get_device_id(hass, event.data.get('entity_id'))
 
+                # If the entity ID is not in the cache, publish the device's metadata and model
                 if entity_id_cache.get(entity_id) is None:
                     attributes = {
                         'thing-metadata': {
@@ -77,18 +164,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                                 'unit': state.attributes.get('unit_of_measurement')
                             }
                         },
-                        'thing-model': f"https://raw.githubusercontent.com/salberternst/thing-models/main/home_assistant/{device_class}.json"
+                        'thing-model': f"{entry.data.get('thing_model_repo_url')}/{device_class}.json"
                     }
 
-                    # we are sending the attributes once
-                    publishAttributes(
+                    message_info = publish_connect(
+                        client=client, device_id=entity_id, device_class=device_class)
+                    message_info.wait_for_publish()
+
+                    publish_attributes(
                         client=client, device_id=entity_id, attributes=attributes)
 
-                    # update the cache
                     entity_id_cache[entity_id] = entity_id
 
-                # send the current state to the thingsbaord
-                publishState(client=client, device_id=entity_id,
+                publish_state(client=client, device_id=entity_id,
                              state=state, device_class=device_class)
 
     hass.bus.async_listen(MATCH_ALL, state_event_listener)
--- thingsboard/config_flow.py
+++ thingsboard/config_flow.py
@@ -1,33 +1,100 @@
 """Config flow for thingsboard integration."""
 from __future__ import annotations
-
-import paho.mqtt.client as mqtt 
-
 import logging
-from typing import Any
-
-import voluptuous as vol
 import queue
-
+from typing import Any
+import paho.mqtt.client as mqtt
+import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
+from homeassistant.const import (
+    CONF_SENSORS
+)
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 import homeassistant.helpers.config_validation as cv
-
+from homeassistant.helpers import selector
 from .const import DOMAIN
+
+home_assistant_device_classes = [
+    "battery",
+    "cold",
+    "connectivity",
+    "door",
+    "garage_door",
+    "gas",
+    "heat",
+    "light",
+    "lock",
+    "moisture",
+    "motion",
+    "moving",
+    "occupancy",
+    "opening",
+    "plug",
+    "power",
+    "presence",
+    "problem",
+    "safety",
+    "smoke",
+    "sound",
+    "update",
+    "vibration",
+    "window",
+    "apparent_power",
+    "aqi",
+    "carbon_dioxide",
+    "carbon_monoxide",
+    "current",
+    "data_rate",
+    "data_size",
+    "distance",
+    "energy",
+    "enum",
+    "frequency",
+    "humidity",
+    "illuminance",
+    "monetary",
+    "nitrogen_dioxide",
+    "nitrogen_monoxide",
+    "nitrous_oxide",
+    "ozone",
+    "pm1",
+    "pm10",
+    "pm25",
+    "power_factor",
+    "pressure",
+    "signal_strength",
+    "speed",
+    "sulphur_dioxide",
+    "temperature",
+    "timestamp",
+    "volatile_organic_compounds",
+    "voltage",
+    "weight",
+    "wind_speed"
+]
+
 
 MQTT_TIMEOUT = 5
 _LOGGER = logging.getLogger(__name__)
 
-# TODO adjust the data schema to the data that you need
-STEP_USER_DATA_SCHEMA = vol.Schema(
+STEP_MQTT_DATA_SCHEMA = vol.Schema(
     {
-        vol.Required("host"): str,
-        vol.Required("port", default=8081): cv.port,
-        vol.Required("access_token", default="thetoken"): str
+        vol.Required("host", default="dataspace.plaiful.org"): str,
+        vol.Required("port", default=8883): cv.port,
+        vol.Required("tls", default=True): bool,
+        vol.Required("access_token"): str,
+        vol.Required("thing_model_repo_url",
+                     default="https://raw.githubusercontent.com/salberternst/thing-models/main/home_assistant"): str,
+        vol.Required(CONF_SENSORS, default=[]): selector.SelectSelector(
+            selector.SelectSelectorConfig(
+                options=home_assistant_device_classes, translation_key=CONF_SENSORS, multiple=True
+            ),
+        ),
     }
 )
+
 
 def try_connection(
     user_input: dict[str, Any],
@@ -35,19 +102,14 @@ def try_connection(
     client = mqtt.Client("home-assistant", protocol=mqtt.MQTTv31, userdata={})
     result: queue.Queue[int] = queue.Queue(maxsize=1)
 
-    def on_connect(
-        client_: mqtt.Client,
-        userdata: None,
-        flags: dict[str, Any],
-        result_code: int,
-        properties: mqtt.Properties | None = None,
-    ) -> None:
-        """Handle connection result."""
+    def on_connect(result_code: int) -> None:
         result.put(result_code)
 
     client.on_connect = on_connect
     client.username_pw_set(user_input['access_token'], password=None)
     client.connect_async(user_input['host'], user_input['port'])
+    if user_input['tls']:
+        client.tls_set()
     client.loop_start()
 
     try:
@@ -58,6 +120,7 @@ def try_connection(
         client.disconnect()
         client.loop_stop()
 
+
 async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
     """Validate the user input allows us to connect."""
     connect_result = await hass.async_add_executor_job(
@@ -66,10 +129,11 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 
     if connect_result == mqtt.CONNACK_ACCEPTED:
         return {"title": data["host"]}
-    elif connect_result == mqtt.AUTH or connect_result == mqtt.CONNACK_REFUSED_BAD_USERNAME_PASSWORD:
+    
+    if connect_result in (mqtt.AUTH, mqtt.CONNACK_REFUSED_BAD_USERNAME_PASSWORD):
         raise InvalidAuth()
-    else:
-        raise CannotConnect()
+    raise CannotConnect()
+
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for thingsboard."""
@@ -95,7 +159,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 return self.async_create_entry(title=info["title"], data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=STEP_MQTT_DATA_SCHEMA, errors=errors
         )
 
 
