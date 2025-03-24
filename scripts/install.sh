d56de3d5b6885d6c21810acbce06ba023a903869
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2025-03-24 11:52:40 +0000

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,6 +15,11 @@ if [ -d "/tmp/ha_thingsboard-main" ]; then
     rm -rf /tmp/ha_thingsboard-main
 fi
 
+if [ -f "/tmp/ha_thingsboard-main.zip" ]; then
+    echo "Removing existing zip file"
+    rm /tmp/ha_thingsboard-main.zip
+fi
+
 wget -O /tmp/ha_thingsboard-main.zip "https://github.com/connected-intelligent-systems/ha_thingsboard/archive/refs/heads/main.zip" && \
     unzip /tmp/ha_thingsboard-main.zip -d /tmp/ && \
     mv /tmp/ha_thingsboard-main/custom_components/thingsboard /config/custom_components/ && \
